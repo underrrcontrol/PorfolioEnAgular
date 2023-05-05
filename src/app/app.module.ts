5cdc33a74d7d69f0e87ed3bd20d92e478f4565ac
underrrcontrol <fabricio_daniele@hotmail.com>
2023-05-05 20:45:11 -0300

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
+import { SobremiComponent } from './componentes/sobremi/sobremi.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { ContactoComponent } from './componentes/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    SobremiComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
